870a3567a53014006741bb26c82162e98e25e914
abc <jarvissilva818@gmail.com>
2021-05-31 11:03:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './screens/Home'
+import Search from './screens/Search'
+import Favourites from './screens/Favourites'
+import Account from './screens/Account'
+
+const App = () => {
+  return (
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/search' component={Search} />
+      <Route exact path='/favourites' component={Favourites} />
+      <Route exact path='/account' component={Account} />
+    </Router>
+  )
+}
+
+export default App
